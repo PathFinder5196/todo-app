26ec22eca7fb66b4f390edf443e50a9748c6dd6d
PathFinder5196 <laveshkat@gmail.com>
2020-03-06 08:59:10 +0530

    Updates

---
+++ precache-manifest.3c0917aa744f8b0d839fc260d3838b14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e69eebde67cabcb5588950bcbeecc3",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "e4f8101869b6579b980b",
+    "url": "/todo-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "210d8a4ee8f43fc7d692",
+    "url": "/todo-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e4f8101869b6579b980b",
+    "url": "/todo-app/static/js/2.5a18512a.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/todo-app/static/js/2.5a18512a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "210d8a4ee8f43fc7d692",
+    "url": "/todo-app/static/js/main.43485e31.chunk.js"
+  },
+  {
+    "revision": "b66ecab6fc712c6d22a7",
+    "url": "/todo-app/static/js/runtime-main.e77e13dd.js"
+  }
+]);
